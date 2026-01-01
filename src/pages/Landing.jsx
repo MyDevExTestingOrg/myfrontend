aa7c2a01c5956725c2ff617b82ea282df8012a5b
prashantpagare44 <prashantpagarejd@gmail.com>
2026-01-01 10:55:34 +0530

    this is  landing page

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,28 @@
+import {useState,useEffect} from 'react'
+import Navbar from '../component/Navbar.jsx'
+import HeroSection from '../component/HeroSection.jsx'
+import Features from '../component/Features.jsx'
+import MarketGap from '../component/MarketGap.jsx'
+import Founder from '../component/Founder.jsx'
+import Footer from '../component/Footer.jsx'
+function Landing() {
+  const [darkMode,setDarkMode] = useState(false);
+
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark", darkMode);
+  }, [darkMode]);
+  
+
+  return (
+    <div  className="min-h-screen bg-white ">
+    <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+    <HeroSection/>
+    <Features/>
+    <MarketGap/>
+    <Founder/>
+    <Footer/>
+    </div>
+  )
+}
+
+export default Landing
