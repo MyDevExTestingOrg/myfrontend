5c1d32ed48f3eece207b6625eca3336f3cd5aa3f
prashantpagare44 <prashantpagarejd@gmail.com>
2026-01-12 13:09:07 +0530

    changes analytics.js

--- src/services/analytics.js
+++ src/services/analytics.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = "http://localhost:3000/api/v1/webhooks"
+const API_URL = `${import.meta.env.VITE_BACKEND_URL}/api/v1/webhooks`
 
 export const fetchCTOMetrics = async(userId)=>{
     try {
